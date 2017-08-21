6cb1bce7f4ab2238afe2ebd5468c348bee427d7c
Xuso Rodriguez <jrodriguez_one@hotmail.es>
2017-08-21 12:13:40 +0200

    Modified main.less, main.js add object, Home_Work_manager add JQuery

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,1 +1,28 @@
 // Main JS File
+
+var Util = {
+    View: {
+        test: function() {
+            console.log("Views Object is Work");
+        }
+    },
+
+    Events: {   
+        onClickNewNote: function() {
+            $('#btn_new_note').click(function() {
+                alert('onClick');
+            });
+        }
+    },
+
+    AjaxOject: {
+        test: function() {
+            console.log("Ajax Object is Work");
+        }
+    },
+
+    // Object Util's functions
+    start: function() {        
+        this.Events.onClickNewNote();
+    }
+}
