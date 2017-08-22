76b55a5be9b7ef35287b67692a737f1d4145a93b
Xuso Rodriguez <jrodriguez_one@hotmail.es>
2017-08-22 15:16:31 +0200

    main.js: Added methods updatePanelNoteList() and makeCard() in Object View. modified method reques() in AjaxObject main.less: added class car-note.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -2,8 +2,31 @@
 
 var Util = {
     View: {
-        test: function() {
-            console.log("Views Object is Work");
+        /**
+         * @param {array} note_list
+         */
+        updatePanelNoteList: function(note_list) {
+            if(Array.isArray(note_list)) { //check note_list
+                var appendCards = '';
+                note_list.forEach(function(value, index) {                    
+                    appendCards += Util.View.makeCard(value);
+                });
+                // Insert content at the end
+                $('.panel-work-list-note').append(appendCards);
+            } else {console.log('ERROR: parameter not is array');}            
+        },
+
+        /**
+         * Returns content html <div class="card-note"></div>
+         * @param {array} note
+         * @returns {string} 
+         */
+        makeCard: function(note) {            
+            return '<div class="card-note">' +
+            '<h3 class="tittle">'+note['tittle']+'</h3>' +
+            '<p class="detail">'+note['details']+'</p>' +
+            '<p class="date">'+note['date_begin']+'</p>' +
+            '</div>';
         }
     },
 
@@ -15,20 +38,29 @@ var Util = {
         }
     },
 
-    AjaxOject: {
-        request: function(parameters, data_type) {
-            $.ajax({
+    AjaxOject: {                
+        /**
+         * Returns a Object ajax
+         * @param {array} parameters - key/value
+         * @param {string} data_type - can be json | text | html ...
+         * @returns {object} 
+         */ 
+        request: function(parameters, data_type) {    
+                  
+            return $.ajax({
                 data: parameters,
                 type: 'POST',
                 dataType: data_type,
                 url: '../server/main.php',
-            })
+            });
+            /*
             .done(function(data, textStatus, jqXHR) {
-                console.log("res: "+ data);
+                
             })
             .fail(function(jqXHR, textStatus, errorThrow) {
-                console.log("ERROR: "+ errorThrow);
+                console.log("ERROR: "+ errorThrow);                              
             });
+            */
         }
     },
 
@@ -36,6 +68,10 @@ var Util = {
     start: function() {        
         this.Events.onClickNewNote();
         //testing ................
-        this.AjaxOject.request({test: 'msg enviado desde el cliente'}, 'text');
+        //this.AjaxOject.request({test: 'msg enviado desde el cliente'}, 'json');         
+        Util.AjaxOject.request({test: 'msg enviado desde el cliente'}, 'json')
+        .done(function(data, textStatus, jqXHR){
+            Util.View.updatePanelNoteList(data);
+        });           
     }
 }
